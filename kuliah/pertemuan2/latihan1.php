ade511c3f6370ca72de0c6e2a263982b69ccfe55
SyahrulYz11 <yazidmuharom011@gmail.com>
2022-02-25 08:24:40 +0700

    mengerjakan latihan pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,57 @@
+<?php 
+// variable
+$nama = "Syahrul Yazid Muharom";
+echo $nama;
+echo "<hr>";
+// Operator
+// aritmatika
+// +, -, *, /
+
+echo 1 + 2 * 3 - 4;
+
+echo "<br>";
+echo 10 / 5;
+
+echo "<hr>";
+
+// Perbandingan 
+// <, >, <=, >=, ==, !=
+
+echo 1 < 2; // true / 1
+echo "<br>";
+echo 10 < 5; // false / 0
+echo "<br>";
+echo 10 == "10"; // true
+
+echo "<hr>";
+// Identitas 
+// ===, !==
+// mengecek nilai beserta tipe datanya
+
+echo 10 === "10";
+
+echo "<hr>";
+
+// Increment / Decrement 
+// Penambahan / Pengurangan 
+// ++, --
+$x = 10;
+echo $x++;
+echo "<br>";
+$x++;
+echo $x;
+
+echo "<hr>";
+// Concat, Penggabung String 
+// .
+
+$nama_depan = "Syahrul";
+$nama_belakang = 'Yazid';
+echo $nama_belakang . $nama_depan;
+
+
+
+
+
+
+?>
